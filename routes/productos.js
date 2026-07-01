e4cb0c2ca4229a352ceab0515c1b39de0ff2f647
jdponce11 <jponce10111@gmail.com>
2026-07-01 16:19:15 -0600

    prueba con ruta usando funcion getActivos de prisma

--- routes/productos.js
+++ routes/productos.js
@@ -1,8 +1,14 @@
 import express from "express";
+import { getActivos } from "../services/productoService.js";
 const router = express.Router();
 
-router.get("/", function (req, res) {
-  res.json([]);
+router.get("/", async function (req, res) {
+  try {
+    const productos = await getActivos(1);
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener productos" });
+  }
 });
 
 export default router;
